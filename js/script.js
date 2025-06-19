649c532570e1f15acb9be58aeadf9f9a66bb4560
MD Tanvirul Islam <dhtanvir1323@gmail.com>
2025-06-19 23:36:26 +0600

    new  updates
     Callback Hell, then chaining

--- js/script.js
+++ js/script.js
@@ -29,10 +29,81 @@
 
 // ===============================================
 // Callback Hell 
+// const parentContainer = document.querySelector(".parent_Container");
+
+// const renderHtml = (data) => {
+// // console.log(data.borders);
+
+//     // Currency extract
+//     // const currency = Object.keys(data.currencies)[0];
+//     const currency = Object.values(data.currencies)[0].name;
+//     //county_Population
+//     const population = data.population;
+//     const renderHtml = `
+//         <div class="inner_container">
+//             <div class="image_flag">
+//                 <img src="${data.flags?.svg}" style="width: 600px; " alt="">
+//             </div>
+//             <div class="content">
+//                 <h1 class="county_name">${data.name.common}</h1>
+//                 <h1 class="county_Population">${formateNumber(population)}</h1>
+//                 <h1 class="county_Currency">${currency}</h1>
+//             </div>
+//         </div>
+//     `;
+//     parentContainer.insertAdjacentHTML("beforeend", renderHtml);
+// }
+// const getCountryData = function (countryName) {
+//     let request = new XMLHttpRequest();
+//     request.open("GET", `https://restcountries.com/v3.1/name/${countryName}`);
+//     request.send();
+
+//     request.addEventListener("load", function () {
+//         let [data] = JSON.parse(this.responseText);
+//         console.log(data);
+//         const borders = data?.borders;
+//         // console.log(borders);
+//         borders?.forEach((border) => {
+//             let borderRequest = new XMLHttpRequest();
+//             borderRequest.open(
+//                 "GET",
+//                 `https://restcountries.com/v3.1/alpha/${border}`
+//             );
+//             // console.log(border);
+
+//             borderRequest.send();
+//             borderRequest.addEventListener("load", function () {
+//                 let [borderData] = JSON.parse(this.responseText);
+//                 renderHtml(borderData);
+//             })
+//         });
+//         renderHtml(data);
+//     });
+// }
+// function formateNumber(num) {
+//     if (num >= 1e9) { return (num / 1e9).toFixed(2) + 'B' }
+//     else if (num >= 1e6) { return (num / 1e6).toFixed(2) + 'M' }
+//     else { return num.toSting() }
+// };
+// // getCountryData("Germany")
+// getCountryData("Pakistan")
+
+// <h1 class="county_Population">${(data.population / 1_000_000).toFixed(1)}M</h1>
+// Callback Hell
+// setTimeout(function(){
+// console.log('hello');
+// },10000)
+
+
+
+// ===================================================
+
+// then chaining
+
 const parentContainer = document.querySelector(".parent_Container");
 
 const renderHtml = (data) => {
-// console.log(data.borders);
+    // console.log(data.borders);
 
     // Currency extract
     // const currency = Object.keys(data.currencies)[0];
@@ -53,47 +124,49 @@ const renderHtml = (data) => {
     `;
     parentContainer.insertAdjacentHTML("beforeend", renderHtml);
 }
-const getCountryData = function (countryName) {
-    let request = new XMLHttpRequest();
-    request.open("GET", `https://restcountries.com/v3.1/name/${countryName}`);
-    request.send();
+//Callback Hell 
+const getCountryDate = (countryName) => {
+    fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+        .then((response) => {
 
-    request.addEventListener("load", function () {
-        let [data] = JSON.parse(this.responseText);
-        console.log(data);
-        const borders = data?.borders;
-        // console.log(borders);
-        borders?.forEach((border) => {
-            let borderRequest = new XMLHttpRequest();
-            borderRequest.open(
-                "GET",
-                `https://restcountries.com/v3.1/alpha/${border}`
-            );
-            // console.log(border);
+            return response.json();
 
-            borderRequest.send();
-            borderRequest.addEventListener("load", function () {
-                let [borderData] = JSON.parse(this.responseText);
-                renderHtml(borderData);
-            })
+        }).then((date) => {
+
+            renderHtml(date[0]);
+
+            const borders = date[0]?.borders;
+
+            borders?.forEach(getBorders);
+
         });
-        renderHtml(data);
-    });
+};
+
+function getBorders(borderName) {
+
+    fetch(`https://restcountries.com/v3.1/alpha/${borderName}`)
+
+        .then((response) => response.json())
+
+        .then((borderDate) => {
+
+            // console.log(borderDate);
+
+            renderHtml(borderDate[0])
+
+        })
+
 }
+
+
+getCountryDate("Bangladesh");
+
 function formateNumber(num) {
     if (num >= 1e9) { return (num / 1e9).toFixed(2) + 'B' }
     else if (num >= 1e6) { return (num / 1e6).toFixed(2) + 'M' }
     else { return num.toSting() }
 };
-// getCountryData("Germany")
-getCountryData("Pakistan")
-
-// <h1 class="county_Population">${(data.population / 1_000_000).toFixed(1)}M</h1>
-// Callback Hell
-// setTimeout(function(){
-// console.log('hello');
-// },10000)
 
 
 
-// ===================================================
+
